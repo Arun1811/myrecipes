96237f7781dad15ba30854f4edf7754e8bf5d242
Arun A <arunpuduvai@gmail.com>
2017-11-16 12:10:57 +0530

    edit and delete recipe added

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = User.create!(username: "Arun 1", email: "arun@example.com")
+		@recipe = Recipe.create(name: "Vegetable saute", description: "greatest Vegetable, add Vegetableand oil", user: @user)
+	end
+
+	test "succesfully delete a recipe" do 
+		get recipe_path(@recipe)
+		assert_template 'recipes/show'
+		assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+		assert_difference  'Recipe.count', -1 do
+			delete recipe_path(@recipe)
+		end
+		assert_redirected_to recipes_path
+		assert_not flash.empty?
+	end
+
+
+end
---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = User.create!(username: "Arun 1", email: "arun@example.com")
+		@recipe = Recipe.create(name: "Vegetable saute", description: "greatest Vegetable, add Vegetableand oil", user: @user)
+	end
+
+	test "reject invalid recipe update" do 
+		get edit_recipe_path(@recipe)
+		assert_template 'recipes/edit'
+		patch recipe_path(@recipe), params: {recipe: {name: " ", description: "some description"}}
+		assert_template 'recipes/edit'
+		assert_select 'h2.panel-title'
+		assert_select 'div.panel-body'
+
+	end
+
+	test "successfully edit recipe" do
+		get edit_recipe_path(@recipe)
+		assert_template 'recipes/edit'
+		update_name = "updated recipe name"
+		updated_description = "udpated description name"
+		patch recipe_path(@recipe), params: {recipe:{name: update_name, description: updated_description}}
+		assert_redirected_to @recipe
+		# follow_redirect!
+		assert_not flash.empty?
+		@recipe.reload
+		assert_match update_name, @recipe.name
+		assert_match updated_description, @recipe.description
+	end
+
+end
